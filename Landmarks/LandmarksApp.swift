692c31508275a8cbfd1e4d7ee062469efafe2431
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-04-20 19:02:53 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ryosuke Tanimura on 2022/04/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
