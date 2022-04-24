8e8ba3c15c2353afbc68aef5f00c29b296f54de3
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-04-24 23:29:22 +0900

    メイン機能を実装（Working with UI Controls）

---
+++ Landmarks/Views/Helpers/MapView.swift
@@ -0,0 +1,33 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Ryosuke Tanimura on 2022/04/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var coodinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .onAppear{
+                setRegion(coodinate)
+            }
+    }
+    
+    private func setRegion(_ coodinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coodinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coodinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+    }
+}
